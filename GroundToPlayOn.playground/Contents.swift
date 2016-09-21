d3d4e7c196162fe6104db692e2737daa6fcd63ee
Pbra1484 <pbra1484@740s-pl-108.local>
2016-09-21 11:43:26 -0600

    Made compatible with github

---
+++ GroundToPlayOn.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+////Sting Change
+
+
+var somthing = "somthijng"
+print(somthing)
+somthing = "somting else"
+print(somthing)
+somthing += "withthis"
+print(somthing)
+
+
+////Interpolation
+
+
+
+
+let this = "That"
+print("this is = to \(this)")
+
+
+
+//// Counting Character
+
+
+
+var count = somthing.characters.count
+somthing += " adding charetsers"
+
+/////Compating strings
+
+if somthing == this
+{
+    print("yes")
+
+}
+else if somthing != this
+{
+    print("no")
+}
---
+++ Playground1.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var thisIsANumber = 23084390;
+print(thisIsANumber);
+print(str + "stuff");
+print(str + "\(thisIsANumber)");
+
+
+let somthing = 1;
+
+print(somthing);
+
+var this = "🐱";
+
+
+print(this);
+
+var fmk = (5 + 5 + 4543509);
+
+let runing = true;
+
+if runing
+{
+print("this device is a potato")
+
+}
+jkjjkjkjkjkkkjl
