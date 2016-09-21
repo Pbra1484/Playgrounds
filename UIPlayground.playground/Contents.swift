0159b66683be6c1e30f49a7e85c36a19f5443df7
Pbra1484 <pbra1484@740s-pl-108.local>
2016-09-21 13:44:18 -0600

    Made UI playground code

--- UIPlayground.playground/Contents.swift
+++ UIPlayground.playground/Contents.swift
@@ -2,3 +2,25 @@
 
 import UIKit
 
+var DynamicView = UIView(frame: CGRectMake(100, 200, 100, 100))
+DynamicView.backgroundColor=UIColor.orangeColor()
+DynamicView.layer.cornerRadius=25
+DynamicView.layer.borderWidth=5
+DynamicView
+var DynamicButton = UIButton(frame: CGRectMake(50, 20, 100, 100))
+
+DynamicButton.backgroundColor=UIColor.greenColor()
+var DynamicButton2 = UIButton(frame: CGRectMake(0, 100, 50, 50))
+DynamicButton2.backgroundColor=UIColor.greenColor()
+var DynamicLable = UILabel(frame: CGRectMake(1, 1, 80, 50))
+DynamicLable.backgroundColor=UIColor.clearColor()
+DynamicLable.text = "\"Lable\""
+var DynamicLable2 = UILabel(frame: CGRectMake(10, 10, 100, 50))
+DynamicLable2.backgroundColor=UIColor(red:1.00, green:0.41, blue:0.71, alpha:1.0)
+DynamicLable2.text = "lable2"
+
+
+
+
+
+
